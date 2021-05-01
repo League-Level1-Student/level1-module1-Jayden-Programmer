6d24aaae0905aabcebb29bd859d82074fc7937ca
jayki <jayki@JayLaptop>
2021-05-01 14:24:40 -0700

    Code_Ididfine

--- src/_06_duck/Duck.java
+++ src/_06_duck/Duck.java
@@ -1,6 +1,19 @@
 package _06_duck;
 
 public class Duck {
+	int numberOfFriends;
+	String favoriteFood;
+	Duck(String favoriteFood, int numberOfFriends) {
+        this.favoriteFood = favoriteFood;
+        this.numberOfFriends = numberOfFriends;
+	}
+	void quack() { 
+	System.out.println("Quack!");
+	 }
+	
 
-
+	void waddle() {
+	System.out.println("Ight imma waddle");
+	}
 }
+                                                 
