86951cd2a9aa437d937a105872d44176fd18b8ad
jayki <jayki@JayLaptop>
2021-05-08 11:25:30 -0700

    Hi_e

--- src/_07_binary_converter/Binaryconverter.java
+++ src/_07_binary_converter/Binaryconverter.java
@@ -5,32 +5,64 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
+import javax.swing.JTextField; 
+  
 
 
 
 
 public class Binaryconverter implements ActionListener {
+	String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+
+	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel(); 
 	JButton button = new JButton(); 
 	JTextField field = new JTextField(20); //what your input in the paranthesis is the size of the JTextField  
-
+	JLabel label = new JLabel();
 public void run() {
 button.addActionListener(this); 
 frame.setVisible(true);
 frame.add(panel);
-frame.pack();
+frame.pack();  
+panel.add(button); 
+panel.add(field);  
+panel.add(label);
+
+
+
+
 
 }
 
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	
-	
+	String binary =  field.getText(); 
+	String save = convert(binary);  
+	label.setText(save); 
+	frame.pack();
 }
 }
 
---
+++ src/_07_binary_converter/Binaryconverterrunner.java
@@ -0,0 +1,12 @@
+package _07_binary_converter;
+
+import javax.swing.JOptionPane;
+
+public class Binaryconverterrunner {
+public static void main(String[] args) { 
+Binaryconverter bin = new Binaryconverter();  
+bin.run();
+
+}
+}
+
